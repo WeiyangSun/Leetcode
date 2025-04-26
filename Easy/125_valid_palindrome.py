cb5095e423723d01f452d5122b0e6c4903e9e84b
weiyangsun <weiyangsun@rivian.com>
2025-04-25 19:16:15 -0700

    refreshed day 116B

--- Easy/125_valid_palindrome.py
+++ Easy/125_valid_palindrome.py
@@ -83,3 +83,21 @@ class ConstantSolution:
             # When encountering non-alphanumeric types, this line helps to skip it
             i, j = i + (not a.isalnum()), j - (not b.isalnum())
         return True
+
+class ConstantSolution:
+    def is_palindrome(self, s: str) -> bool:
+        left_pointer, right_pointer = 0, len(s)-1
+
+        while left_pointer < right_pointer:
+            while left_pointer < right_pointer and not s[left_pointer].isalnum():
+                left_pointer += 1
+            while left_pointer < right_pointer and not s[right_pointer].isalum():
+                right_pointer -= 1
+
+            if s[left_pointer].lower() != s[right_pointer].lower():
+                return False
+            
+            left_pointer += 1
+            right_pointer += 1
+
+        return True
