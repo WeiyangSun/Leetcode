08af0e4b348ce0f7fe26fc20f3a7fbbd1735d96e
weiyangsun <weiyangsun@rivian.com>
2025-05-08 22:31:50 -0500

    refreshed day 124

---
+++ Medium/1424_diagonal_traverse_ii.py
@@ -0,0 +1,34 @@
+"""
+1424. Diagonal Traverse II
+
+Given a 2D integer array nums, return all elements of nums in diagonal order as shown in the below
+images.
+"""
+
+"""
+Example 1:
+Input: nums = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,4,2,7,5,3,8,6,9]
+
+Example 2:
+Input: nums = [[1,2,3,4,5],[6,7],[8],[9,10,11],[12,13,14,15,16]]
+Output: [1,6,2,8,7,3,9,4,12,10,5,13,11,14,15,16]
+"""
+
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def findDiagonalOrder(self, nums:List[List[int]]) -> List[int]:
+        lanes = defaultdict(list)
+        for row_ix, row in enumerate(nums):
+            for col_ix, value in enumerate(row):
+                lane_id = row_ix + col_ix
+                lanes[lane_id].append(value)
+
+        result = []
+        for lane_id in range(max(lanes) + 1):
+            if lane_id in lanes:
+                result.extend(reversed(lanes[lane_id]))
+
+        return result
